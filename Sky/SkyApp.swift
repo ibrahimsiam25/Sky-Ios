999b10a9c3999cec09507ff2fd1f6e06ee73d6cf
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-05-28 15:56:58 +0300

    Initial Commit

---
+++ Sky/SkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyApp.swift
+//  Sky
+//
+//  Created by siam on 28/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
